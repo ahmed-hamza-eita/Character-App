64ca9e697504ccefdb78efe17991d9963f0962ef
aheit <aeita4@yahoo.com>
2025-07-25 15:39:07 +0300

    add hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -45,4 +47,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.dagger:hilt-android:2.48")
+    ksp("com.google.dagger:hilt-android-compiler:2.48")
 }
--- app/src/main/java/com/hamza/characterapp/MyApp.kt
+++ app/src/main/java/com/hamza/characterapp/MyApp.kt
@@ -1,5 +1,7 @@
 package com.hamza.characterapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MyApp : Application()
