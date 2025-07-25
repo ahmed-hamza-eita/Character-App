f39cb020015a6669ac3158269aad2f532ea73cf9
aheit <aeita4@yahoo.com>
2025-07-25 16:06:43 +0300

    model

---
+++ app/src/main/java/com/hamza/characterapp/data/model/CharacterModel.kt
@@ -0,0 +1,30 @@
+package com.hamza.characterapp.data.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CharacterModel(
+    @SerializedName("info") val info: Info, @SerializedName("results") val results: List<Result>
+) {
+    data class Info(
+        @SerializedName("count") val count: Int,
+        @SerializedName("next") val next: String,
+        @SerializedName("pages") val pages: Int,
+        @SerializedName("prev") val prev: Any
+    )
+
+    data class Result(
+        @SerializedName("created") val created: String,
+        @SerializedName("episode") val episode: List<String>,
+        @SerializedName("gender") val gender: String,
+        @SerializedName("id") val id: Int,
+        @SerializedName("image") val image: String,
+
+        @SerializedName("name") val name: String,
+
+        @SerializedName("species") val species: String,
+        @SerializedName("status") val status: String,
+        @SerializedName("type") val type: String,
+        @SerializedName("url") val url: String
+    )
+}
