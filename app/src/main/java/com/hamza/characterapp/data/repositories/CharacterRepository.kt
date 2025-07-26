16c478d102cac3b315d7f91bf167247e832e8251
aheit <aeita4@yahoo.com>
2025-07-26 04:32:35 +0300

    repositories

---
+++ app/src/main/java/com/hamza/characterapp/data/repositories/CharacterRepository.kt
@@ -0,0 +1,41 @@
+package com.hamza.characterapp.data.repositories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.hamza.characterapp.data.model.CharacterResponse
+import com.hamza.characterapp.data.retrofit.ApiCalls
+import com.hamza.characterapp.utils.Resource
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+class CharacterRepository @Inject constructor(private val api: ApiCalls) {
+    private val _characterLiveData = MutableLiveData<Resource<CharacterResponse>>()
+    val characterLiveData: LiveData<Resource<CharacterResponse>>
+        get() = _characterLiveData
+
+    fun getAllCharacters() {
+        _characterLiveData.postValue(Resource.loading())
+
+        val callData = api.getAllCharacters()
+        callData.enqueue(object : Callback<CharacterResponse> {
+            override fun onResponse(
+                call: Call<CharacterResponse?>, response: Response<CharacterResponse?>
+            ) {
+                if (response.body() != null) {
+                    _characterLiveData.postValue(Resource.success(response.body()!!))
+                } else {
+                    _characterLiveData.postValue(Resource.error("Network Error"))
+                }
+            }
+
+            override fun onFailure(
+                call: Call<CharacterResponse?>, t: Throwable
+            ) {
+                _characterLiveData.postValue(Resource.error("Error: ${t.message.toString()}"))
+            }
+
+        })
+    }
+}
