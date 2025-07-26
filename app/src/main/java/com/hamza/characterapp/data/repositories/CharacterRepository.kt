192af9a68b59cf9663614c5ffda50a141385a355
aheit <aeita4@yahoo.com>
2025-07-26 15:45:11 +0300

    repositories

--- app/src/main/java/com/hamza/characterapp/data/repositories/CharacterRepository.kt
+++ app/src/main/java/com/hamza/characterapp/data/repositories/CharacterRepository.kt
@@ -11,13 +11,14 @@ import retrofit2.Response
 import javax.inject.Inject
 
 class CharacterRepository @Inject constructor(private val api: ApiCalls) {
+
     private val _characterLiveData = MutableLiveData<Resource<CharacterResponse>>()
     val characterLiveData: LiveData<Resource<CharacterResponse>>
         get() = _characterLiveData
 
+
     fun getAllCharacters() {
         _characterLiveData.postValue(Resource.loading())
-
         val callData = api.getAllCharacters()
         callData.enqueue(object : Callback<CharacterResponse> {
             override fun onResponse(
@@ -35,6 +36,37 @@ class CharacterRepository @Inject constructor(private val api: ApiCalls) {
             ) {
                 _characterLiveData.postValue(Resource.error("Error: ${t.message.toString()}"))
             }
+
+        })
+    }
+
+    //get one character
+
+    private val _oneCharacterLiveData = MutableLiveData<Resource<CharacterResponse.Character>>()
+    val oneCharacterLiveData: LiveData<Resource<CharacterResponse.Character>>
+        get() = _oneCharacterLiveData
+
+    fun getOneCharacters(id: String) {
+        _oneCharacterLiveData.postValue(Resource.loading())
+        val callData = api.getSingleCharacter(id)
+        callData.enqueue(object : Callback<CharacterResponse.Character> {
+            override fun onResponse(
+                call: Call<CharacterResponse.Character?>,
+                response: Response<CharacterResponse.Character?>
+            ) {
+                if (response.body() != null) {
+                    _oneCharacterLiveData.postValue(Resource.success(response.body()!!))
+                } else {
+                    _oneCharacterLiveData.postValue(Resource.error("Network Error"))
+                }
+            }
+
+            override fun onFailure(
+                call: Call<CharacterResponse.Character?>, t: Throwable
+            ) {
+                _oneCharacterLiveData.postValue(Resource.error("Error: ${t.message.toString()}"))
+            }
+
 
         })
     }
